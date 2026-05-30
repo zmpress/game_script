619f618f1cc4aa0d7209063d7218aa910063a4cc
zhuchen <1204178674@qq.com>
2026-05-30 12:41:16 +0800

    OCFacilitation 修改默认配置

--- porn_city/userscript/OCFacilitation.js
+++ porn_city/userscript/OCFacilitation.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         OC进度显示和Cooldowns(可设置)
+// @name         OC进度显示和Cooldowns(可设置，兼容PDA)
 // @version      1.0
 // @description  显示 oc 进度，显示drug，medical，booster 的剩余时间，显示 refill 信息，均可设置是否显示
 // @author       zmpress [3633431]
